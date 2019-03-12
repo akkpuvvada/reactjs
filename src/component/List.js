b26c11b046291c61f240cc701bb68a7373c5d58f
Garuda <akk.puvvada@gmail.com>
2019-03-12 20:50:32 +0530

    1) Added the form that takes input of some fields 2) Corrected the list

--- src/component/List.js
+++ src/component/List.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDataGrid from 'react-data-grid';
 
 var row = [];
+var tempRows = [];
 
 const columns = [{
     key: 'equipId',
@@ -21,49 +22,69 @@ class List extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            rows: []
+            rows: [],
+            equipValue: "",
+            vechId:"",
         };
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     componentDidMount() {
-        var responseText = require('../data/response.json');
-        responseText.map((data)=>{
-            row = []
-            row = [{equipId:data.equipment,
-                    timeStamp: data.time,
-                    vechId: data.vehicle}]
-            //this.setState({rows:[...this.state.rows,row]})
-            var joined = this.state.rows.concat(row);
-            this.setState({ rows: joined })
-        })
+        
     }
 
-    rowGetter=(rowNumber)=>{
-        if(this.state.rows[rowNumber]!==undefined)
-        {
-        return this.state.rows[rowNumber]
+    rowGetter = (rowNumber) => {
+        if (this.state.rows[rowNumber] !== undefined) {
+            return this.state.rows[rowNumber]
         }
     }
 
-    clickedRow = (rowNumber) =>{
-        console.log(rowNumber)
+    clickedRow = (rowNumber) => {
+        alert("Clicked on row " + this.state.rows[rowNumber].equipId)
     }
 
+    handleSubmit(event) {
+        event.preventDefault();
+        var responseText = require('../data/response.json');
+        tempRows = []
+        responseText.map((data) => {
+            row = []
+            row = {
+                equipId: data.equipment,
+                timeStamp: data.time,
+                vechId: data.vehicle
+            }
+            tempRows.push(row)
+        })
+        this.setState({ rows: tempRows })
+      }
+
     render() {
-        console.log(this.state.rows)
         return (
             <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                    Equipment:
+                    <input type="text" ref="equipment"/>
+                    </label>    
+
+                    <label>
+                    Vechicle:
+                    <input type="text" ref="vehicle"/>
+                    </label>
+
+                    <input type="submit" value="Submit" />
+                </form>
+                <br/>
                 <ReactDataGrid
-                    columns = {columns}
-                    rowGetter = {this.rowGetter}
-                    rowsCount = {this.state.rows.length}
-                    minHeight = {500}
-                    onRowClick = {(e)=>this.clickedRow(e)}
+                    columns={columns}
+                    rowGetter={this.rowGetter}
+                    rowsCount={this.state.rows.length}
+                    minHeight={500}
+                    onRowClick={(e) => this.clickedRow(e)}
                 />
             </div>
         );
-
-
     }
 }
 
